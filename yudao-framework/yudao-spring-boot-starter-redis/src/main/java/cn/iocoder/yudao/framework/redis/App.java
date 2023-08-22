8005f99003591268c05dc856e5caca2291027c2f
Dawson <dawsongong71@gmail.com>
2023-08-22 17:49:13 +0800

    ci: 用例测试配置

---
+++ yudao-framework/yudao-spring-boot-starter-redis/src/main/java/cn/iocoder/yudao/framework/redis/App.java
@@ -0,0 +1,22 @@
+/**
+ * Project Name:yudao-cloud
+ * Package Name:cn.iocoder.yudao.framework.redis
+ * Date:2023/8/2114:44
+ */
+package cn.iocoder.yudao.framework.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: (描述)
+ * Date: 2023/8/2114:44
+ * @author 姓名
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
